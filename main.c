e8976e507da0927b5c10242c987843491fae90d1
stepin104418 <69006493+stepin104418@users.noreply.github.com>
2020-09-16 22:42:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "primeNumber.h"
+
+int main(void)
+{
+	int n,p;
+	printf("enter positive number to be checked");
+	scanf("%d",&n);
+	p = primeNumber(n);
+	switch(p)
+	{
+    case 1:
+        printf("prime");
+    case 0:
+        printf("negative number");
+    case -1:
+        printf("not prime");
+	}
+	return 0;
+}
